4cbb315c8a1b7b2dbbb26020e28334d9dd0f9076
DongChuan_12 <506584418@qq.com>
2018-06-21 15:16:45 +0800

    第二版  优化后

--- list-30/main.js
+++ list-30/main.js
@@ -1,19 +1,15 @@
-$('.images > img:nth-child(1)').addClass('current')
-$('.images > img:nth-child(2)').addClass('enter')
-$('.images > img:nth-child(3)').addClass('enter')
-
 let n = 1
 
+init()  // 初始化
+
 setInterval(function(){
-	$(`.images > img:nth-child(${x(n)})`).removeClass('current').addClass('leave')
-        .one('transitionend',(e) => {
-	    $(e.currentTarget).removeClass('leave').addClass('enter')
-
+	makeLeave($(`.images > img:nth-child(${x(n)}`)).one('transitionend',(e) => {
+	    makeEnter($(e.currentTarget))
 	})
-	$(`.images > img:nth-child(${x(n + 1)})`).removeClass('enter').addClass('current')
-
+	makeCurrent($(`.images > img:nth-child(${x(n + 1)})`))
 	n = n + 1
 },3000)
+
 
 function x(n){
   if(n>3){
@@ -24,3 +20,23 @@ function x(n){
   } // n = 1 2 3
   return n
 }
+
+function init(){
+    $('.images > img:nth-child(1)').addClass('current')
+    .siblings().addClass('enter')
+}
+
+
+function makeLeave($node){
+    $node.removeClass('current').addClass('leave')
+    return $node
+}
+
+function makeCurrent($node){
+    $node.removeClass('enter').addClass('current')
+}
+
+function makeEnter($node){
+    $node.removeClass('leave').addClass('enter')
+}
+
