1891d1383d416e7448837fa2e05220f07616fd69
DongChuan_12 <506584418@qq.com>
2018-06-26 15:28:58 +0800

    使用Promise实现jQuery.ajax的链式调用

--- list-35/packaging/main.js
+++ list-35/packaging/main.js
@@ -1,26 +1,29 @@
 window.jQuery = function(){}
 
 window.jQuery.ajax = function(options){
-  let {url, method, body, headers, success, fail} = options
+  return new Promise((resolve, reject)=>{
+    let {url, method, body, headers} = options
 
-  let request = new XMLHttpRequest()
-  request.open(method, url)
-  for(let key in headers){
-    let value = headers[key]
-    request.setRequestHeader(key, value)
-  }
-  request.onreadystatechange = function(){
-    if(request.readyState === 4){
-      if(request.status >= 200 && request.status < 300){
-          success.call(undefined, request.responseText)
-      }else{
-          fail.call(undefined, request.responseText)
-      }
-    }    
-  }
+    let request = new XMLHttpRequest()
+    request.open(method, url)
+    for(let key in headers){
+      let value = headers[key]
+      request.setRequestHeader(key, value)
+    }
+    request.onreadystatechange = function(){
+      if(request.readyState === 4){
+        if(request.status >= 200 && request.status < 300){
+          // success.call(undefined, request.responseText)
+          resolve.call(undefined, request.responseText)
+        }else{
+          // fail.call(undefined, request.responseText)
+          reject.call(undefined, request)
+        }
+      }    
+    }
  
-  request.send(body)
- 
+    request.send(body) 
+  })
 }
 button.addEventListener('click', (e) => {
     jQuery.ajax({
@@ -28,12 +31,16 @@ button.addEventListener('click', (e) => {
       method: 'GET',
       headers:{
         'Content-Type':'text/json;charset:utf-8'
-      },
-      success: (text)=>{
-          console.log(text)
-      },
-      fail: (request)=>{
-          console.log(request)
       }
-    })
+    }).then(
+        (text)=>{
+            console.log(text)
+            return '成功'},
+        (request)=>{
+            console.log(request)
+            return 'error1'}
+    ).then(
+        (text)=>{console.log(text)},
+        (request)=>{console.log(request)}
+    )
 })
