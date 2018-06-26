b4a06e13736f41e5440f6d2f0452e9a99b8c1184
DongChuan_12 <506584418@qq.com>
2018-06-26 15:12:09 +0800

    使用ES5封装jQuery.ajax

---
+++ list-35/packaging/main.js
@@ -0,0 +1,44 @@
+window.jQuery = function(){}
+
+window.jQuery.ajax = function(options){
+  let url = options.url
+  let method = options.method
+  let body = options.body
+  let headers = options.headers
+  let success = options.success
+  let fail = options.fail 
+
+  let request = new XMLHttpRequest()
+  request.open(method, url)
+  for(let key in headers){
+    let value = headers[key]
+    request.setRequestHeader(key, value)
+  }
+  request.onreadystatechange = function(){
+    if(request.readyState === 4){
+      if(request.status >= 200 && request.status < 300){
+          success.call(undefined, request.responseText)
+      }else{
+          fail.call(undefined, request.responseText)
+      }
+    }    
+  }
+ 
+  request.send(body)
+ 
+}
+button.addEventListener('click', (e) => {
+    jQuery.ajax({
+      url: './xxx',
+      method: 'GET',
+      headers:{
+        'Content-Type':'text/json;charset:utf-8'
+      },
+      success: (text)=>{
+          console.log(text)
+      },
+      fail: (request)=>{
+          console.log(request)
+      }
+    })
+})
